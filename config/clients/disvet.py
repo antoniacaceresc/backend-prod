4cf1f33ce788dee8a1e5ccca532bdde8bc4e3196
antoniacaceresc <antoniacacerescorrea@gmail.com>
2025-08-14 15:12:23 -0400

    Update disvet.py

--- config/clients/disvet.py
+++ config/clients/disvet.py
@@ -7,7 +7,7 @@ class DisvetConfig:
     AGRUPAR_POR_PO = False
 
     # Parámetros de Optimización
-    VCU_MIN = 0.85
+    VCU_MIN = 0.9
     MAX_ORDENES = 50
     MAX_PALLETS_CONF = 60
     MAX_PALLETS_REAL = 120
