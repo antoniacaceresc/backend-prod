4455a0a5dbddc1fbc77315e1519dd0a849d975e6
antoniacaceresc <antoniacacerescorrea@gmail.com>
2025-11-17 14:27:19 -0300

    Update disvet.py

--- config/clients/disvet.py
+++ config/clients/disvet.py
@@ -63,7 +63,7 @@ class DisvetConfig:
 
     # Tipos de camiones
     TRUCK_TYPES = {
-        'normal': {'cap_weight':23000,'cap_volume':70000, 'max_positions':30,'levels':2,'vcu_min':0.90,'max_pallets':59},
+        'normal': {'cap_weight':23000,'cap_volume':70000, 'max_positions':29.5,'levels':2,'vcu_min':0.90,'max_pallets':59},
         'bh':     {'cap_weight':23000,'cap_volume':70000, 'max_positions':28,'levels':2,'vcu_min':0.55,'max_pallets':55}
     }
 
